c8bf70386510781df1cb2d0baafe5ef7126f1fc8
간식time <tth8181@gmail.com>
2023-07-15 16:44:18 +0000

    Create tsconfig.json /src/app.ts

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/' ,(req, res, next) => {
+	res.send('hello');
+})
+
+app.listen(8000, () => {
+	console.log('server running');
+})
