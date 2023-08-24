a74c2d55b9df2e88192bf7e5d9fa5b22c1421236
간식time <tth8181@gmail.com>
2023-08-24 10:34:36 +0000

    Modify itemDetail page

---
+++ public/item/itemDetail.js
@@ -0,0 +1,29 @@
+function getPercent(){
+	
+	let percentInput = document.getElementById('percent_input');
+
+	const div = document.querySelector(".product-price");
+	let span = div.querySelector('span');
+	if(span){
+		div.removeChild(span);
+	}
+
+	const percent = percentInput.value;
+	
+	let price = document.getElementById('price').innerHTML;
+	
+	span = document.createElement('span');
+	span.innerHTML = `${price * percent}₩`;
+	div.insertBefore(span, div.firstChild);
+}
+
+const inputElement = document.getElementById('percent_input');
+
+    inputElement.addEventListener('input', function() {
+      const inputValue = parseInt(inputElement.value);
+      const maxValue = parseInt(inputElement.getAttribute('max'));
+
+      if (inputValue > maxValue) {
+        inputElement.value = maxValue;
+      }
+    });
