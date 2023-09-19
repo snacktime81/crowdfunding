060a4e6946f2eee85189f5ebd4f9286da060f1d1
간식time <tth8181@gmail.com>
2023-09-19 13:36:56 +0000

    Modify location fetch post item/order

--- public/item/payment.js
+++ public/item/payment.js
@@ -6,7 +6,24 @@ function requestPay() {
 	const priceNumber = Number(price.substring(0, price.length-1))
 
 	//console.log(priceNumber)
+	const form = document.getElementById('form');
 
+			
+	const totalPrice = document.getElementsByClassName('totalPrice')[0].innerHTML;
+	//console.log(totalPrice.substring(0, totalPrice.length - 1))
+	const itemId = window.location.pathname.substring(6);
+	let formData = new FormData(form);
+	formData.append('itemId', itemId);
+	formData.append('price', totalPrice);
+	
+	let object = {}
+	formData.forEach(function(value,key){
+		object[key]=value
+	})
+
+	let data = JSON.stringify(object)
+	console.log(data)
+	
 	IMP.request_pay({
 		pg: "html5_inicis",
 		pay_method: "card",
@@ -14,9 +31,17 @@ function requestPay() {
 		name: itemName,
 		amount: priceNumber,
 			
-	}, function(rsp) {
+	}, async function (rsp) {
 		if(rsp.success){
 			alert('결제가 완료되었습니다');
+			const response = await fetch("/item/order", {
+				method: "POST",
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: data
+			});
+		
 			location.href = '/';
 		}
 		else{
