afb23f80474fff6bd121e43c1dc270192d526ea4
간식time <tth8181@gmail.com>
2023-09-20 10:11:09 +0000

    Create /qAndA router

--- routes/page.ts
+++ routes/page.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import{
-	renderMainNotLoggedIn, renderLogin, renderJoin, renderMainLoggedIn, logout,
+	renderMainNotLoggedIn, renderLogin, renderJoin, renderMainLoggedIn, logout, renderQandA
 } from '../controllers/page';
 
 import{
@@ -16,6 +16,6 @@ router.get('/', isLoggedIn, renderMainLoggedIn);
 router.get('/login', renderLogin);
 router.get('/join', renderJoin);
 router.get('/logout', logout);
-
+router.get('/qAndA', renderQandA);
 
 export default router;
