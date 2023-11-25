2ff27a836f4cec53efb98cfed12a1d55b8d2a8f4
간식time <tth8181@gmail.com>
2023-11-25 13:17:51 +0000

    Modify url

--- public/user.ts
+++ public/user.ts
@@ -1,10 +1,12 @@
 
 async function userDelete() {
+  try{
     const confirmPassword: string | null = prompt("삭제를 위해서 비밀번호를 입력해 주세요");
     const userId: string = window.location.pathname;
-    console.log("유저아이디", userId)
+    const url = `/auth${userId}`;
+    console.log(url)
 
-    let response = await fetch(`/auth/${userId}`, {
+    let response = await fetch(url, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json;charset=utf-8'
@@ -15,6 +17,9 @@ async function userDelete() {
       });
       
       let result = await response.json();
-      alert(result.message);
-
+      alert(result);
+    }
+    catch(err){
+      console.log(err)
+    }
 }
