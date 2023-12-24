e4568d4fe37654c607e0545c57e22123b54bfc42
간식time <tth8181@gmail.com>
2023-12-24 13:18:21 +0000

    Create itemIdToUserIdCheck middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,9 +1,13 @@
 import {RequestHandler, Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
+import {FieldPacket} from 'mysql2/promise';
 
 import {verify, isExpired} from '../func/token';
 import { NoMatchId } from '../types/error';
 import {payload} from "../types/model";
+import { item } from '../types/model';
+import pool from "../models/db";
+
 
 const tokenCheck: RequestHandler = (req, res, next) => {
 	try{
@@ -84,4 +88,36 @@ const idCheck: RequestHandler = (req, res, next) => {
 		next(err);
 	}
 }
-export {tokenCheck, idCheck}
+
+const itemIdToUserIdCheck: RequestHandler = async(req, res, next) => {
+	try{
+		const accessToken: string = req.cookies.accessToken;
+		const accessSecret = process.env.ACCESS_SECRET || '';
+
+		const accessData : payload | 'expired' = verify(accessToken, accessSecret) as payload | 'expired';
+		const itemId: number = req.params.id as unknown as number;
+
+		let query = 'SELECT ITEM.id AS item_id, USER.id as user_id, ITEM.name as item_name, price, percent, explanation, img, deadline, views, USER.name as user_name FROM ITEM INNER JOIN USER ON ITEM.user_ID = USER.id WHERE ITEM.ID = (?);';
+		const [items, fields]:[item[], FieldPacket[]] = await pool.query(query, itemId);
+		const item = items[0];
+
+		if(!isExpired(accessData)){ // 유효한 경우
+			if(accessData.id == item.user_id){
+				next();
+			}
+			else{
+				const error = new NoMatchId('권한이 없습니다.');
+				throw(error);
+			}
+		}
+		else{
+			const error = new Error('로그인을 다시 해주세요');
+			throw(error);
+		}
+		
+	}
+	catch(err){
+		next(err);
+	}
+}
+export {tokenCheck, idCheck, itemIdToUserIdCheck}
