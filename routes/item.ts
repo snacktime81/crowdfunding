2863cc52db41dcc389949548d734a354729d4680
간식time <tth8181@gmail.com>
2023-12-24 13:18:41 +0000

    Create /item/edit/:id router

--- routes/item.ts
+++ routes/item.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 
 import{
-	renderItemList, renderItem, postItem, renderItemId, postOrder, renderMyItemList
+	renderItemList, renderItem, postItem, renderItemId, postOrder, renderMyItemList, renderMyItemId
 } from '../controllers/item';
-import{idCheck, tokenCheck} from '../middlewares/auth';
+import{idCheck, tokenCheck, itemIdToUserIdCheck} from '../middlewares/auth';
 
 const router = express.Router();
 
 router.get('/', tokenCheck, renderItem);
-
 router.post('/', tokenCheck, postItem);
+
+router.get('/edit/:id', tokenCheck, itemIdToUserIdCheck, renderMyItemId)
 
 router.get('/list', renderItemList);
 router.get('/list/:id', tokenCheck, idCheck, renderMyItemList);
