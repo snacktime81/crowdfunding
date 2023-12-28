11f8d39556d36260d081cdfcf767991c6ae48c8e
간식time <tth8181@gmail.com>
2023-12-28 15:25:25 +0000

    Create itemDelete

--- public/item/item.js
+++ public/item/item.js
@@ -1,8 +1,15 @@
 const form = document.getElementById('item_content');
 
-form.addEventListener('submit', async (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   itemPut();
+})
+
+const button = document.getElementById('delete');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  itemDelete();
 })
 
 async function itemPut() {
@@ -29,4 +36,14 @@ async function itemPut() {
   catch (err) {
     console.log(err)
   }
+}
+
+async function itemDelete() {
+  const itemId = window.location.pathname.substring(11);
+  const url = '/item/'.concat(itemId);
+  const response = await fetch(url, {
+    method: "DELETE",
+  })
+  alert('삭제 되었습니다.');
+  window.location.href=`/`;
 }
