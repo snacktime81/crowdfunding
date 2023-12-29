07a19d9804627fed984fd15996506520b4918e8c
간식time <tth8181@gmail.com>
2023-12-29 02:10:33 +0000

    Modify post랑 put에 버튼에 따라 생성과 변경

--- public/item/item.js
+++ public/item/item.js
@@ -2,7 +2,13 @@ const form = document.getElementById('item_content');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  itemPut();
+  const buttonType = document.querySelector('button');
+  if(buttonType.id == 'put'){
+    itemPut();
+  }
+  else{
+    itemPost();
+  }
 })
 
 const button = document.getElementById('delete');
@@ -31,6 +37,32 @@ async function itemPut() {
       body: data
     });
     alert('변경 되었습니다.');
+    window.location.href=url;
+  }
+  catch (err) {
+    console.log(err)
+  }
+}
+
+async function itemPost() {
+  try {
+    const url = window.location.pathname;
+    let formData = new FormData(form);
+
+    const object = {};
+    formData.forEach(function (value, key) {
+      object[key] = value
+    })
+    let data = JSON.stringify(object)
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    });
+    alert('생성 되었습니다.');
     window.location.href=url;
   }
   catch (err) {
